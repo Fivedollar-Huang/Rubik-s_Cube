1baef0b8a495bdabeb79655fa0709ea6f2987db6
FiveDollar <fulongh1@uci.edu>
2021-01-18 19:42:11 -0800

    Added Cube Rotation (as a whole)
    Still have bug with reset with right click, but not a big deal for now.

---
+++ Rubik's Cube/Assets/Scripts/CubeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public float sensitive = 0.002f;
+    private bool left_click = false;
+
+    SpinWholeCube SpinCube;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpinCube = GetComponent<SpinWholeCube>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkUserInput();
+    }
+
+    void checkUserInput()
+    {
+        if (Input.GetMouseButtonDown(0)) left_click = true;
+        else if (Input.GetMouseButtonUp(0)) left_click = false;
+        else if (Input.GetMouseButtonDown(1)) SpinCube.rightClick();
+        else if (Input.GetMouseButtonUp(1)) SpinCube.rightRelease();
+        else if (Input.GetMouseButtonDown(2)) SpinCube.scrollClick();
+
+        if (left_click)
+        {
+        }
+
+    }
+}
---
+++ Rubik's Cube/Assets/Scripts/SpinWholeCube.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinWholeCube : MonoBehaviour
+{
+    public float sensitive = 200f;
+
+    private Vector2 mouse_direction;
+    private Vector3 target_rotation;
+
+    private bool fix_rotation = false;
+
+    private bool right_drag = false;
+    private bool right_click = false;
+    private bool scroll_click = false;
+
+
+    void Update()
+    {
+        if (right_click)
+        {
+            mouse_direction = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            if (mouse_direction != Vector2.zero)
+            {
+                right_drag = true;
+            }
+        }
+        if (scroll_click)
+        {
+            resetRotation();
+        }
+        if (right_drag)
+        {
+            rotateCube();
+        }
+        if (fix_rotation)
+        {
+            fixRotation();
+        }
+        
+    }
+    
+    public void rightClick()
+    {
+        right_click = true;
+    }
+    public void rightRelease()
+    {
+        if (!right_drag) fix_rotation = true;
+        right_click = false;
+        right_drag = false;
+    }
+    public void scrollClick()
+    {
+        scroll_click = true;
+    }
+
+
+
+    void rotateCube()
+    {
+        target_rotation = new Vector3();
+        target_rotation.x += -mouse_direction.y;
+        target_rotation.y += -mouse_direction.x;
+        target_rotation.z += mouse_direction.y;
+
+        transform.Rotate(target_rotation * sensitive * Time.deltaTime, Space.World);
+    }
+
+    void fixRotation()
+    {
+        target_rotation = transform.rotation.eulerAngles;
+        roundAngle();
+        if (transform.rotation.eulerAngles == target_rotation)
+        {
+            fix_rotation = false;
+            return;
+        }
+        transform.rotation = Quaternion.RotateTowards(transform.rotation,
+                                                      Quaternion.Euler(target_rotation),
+                                                      sensitive * Time.deltaTime);
+    }
+
+    void roundAngle()
+    {
+        target_rotation.x = Mathf.Round(target_rotation.x / 90) * 90;
+        target_rotation.y = Mathf.Round(target_rotation.y / 90) * 90;
+        target_rotation.z = Mathf.Round(target_rotation.z / 90) * 90;
+    }
+
+    void resetRotation()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation,
+                                                      Quaternion.Euler(Vector3.zero),
+                                                      2 * sensitive * Time.deltaTime);
+        if (transform.rotation.eulerAngles == Vector3.zero)
+            scroll_click = false;
+    }
+}
+
